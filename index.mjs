e9336ac41b9b51d7ae8a047216cd756fb73cb155
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-20 18:08:18 -0500

    Refactor routes and remove commented code

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 // // import adminRoutes from "./routes/routers.mjs";
 // // import oceanDataRoutes from "./routes/routers.mjs";
-// import test from "./routes/routers.mjs";
+import test from "./routes/routers.mjs";
 import serverless from "serverless-http";
 const app = express();
 app.use(express.json());
@@ -9,10 +9,10 @@ app.use(express.json());
 // // app.use("/wavewise-backend", adminRoutes);
 // // app.use("/wavewise-backend", oceanDataRoutes);
 
-// app.use("/", test);
+app.use("/", test);
 
-app.get("/wavewise-backend", (req, res) => {
-  res.json({ message: "Welcome to WaveWise Backend" });
-});
+// app.get("/wavewise-backend", (req, res) => {
+//   res.json({ message: "Welcome to WaveWise Backend" });
+// });
 
 export const handler = serverless(app);
--- routes/routers.mjs
+++ routes/routers.mjs
@@ -1,18 +1,19 @@
-// routes/adminRoutes.ts
+import express from "express";
+import { authenticateAdmin } from "../controller/authController.mjs";
+import {
+  addOceanData,
+  getOceanData,
+} from "../controller/oceanDataController.mjs";
+const router = express();
 
-// import express from "express";
-// import { authenticateAdmin } from "../controller/authController.mjs";
-// import {
-//   addOceanData,
-//   getOceanData,
-// } from "../controller/oceanDataController.mjs";
+router.post("/admin/authenticate", authenticateAdmin);
 
-// const router = express.Router();
+router.post("/ocean-data", addOceanData);
 
-// router.post("/admin/authenticate", authenticateAdmin);
+router.get("/ocean-data", getOceanData);
 
-// router.post("/ocean-data", addOceanData);
-
-// router.get("/ocean-data", getOceanData);
+router.get("/wavewise-backend", (req, res) => {
+  res.json({ message: "Welcome to WaveWise Backend" });
+});
 
 export default router;
