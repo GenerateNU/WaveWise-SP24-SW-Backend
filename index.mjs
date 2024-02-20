1df7772332bce42239f8c245ca8788425fbd8fc6
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-20 16:36:36 -0500

    Add test route and welcome message

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import adminRoutes from "./routes/routers";
 import oceanDataRoutes from "./routes/routers";
+import test from "./routes/routers";
 import serverless from "serverless-http";
 
 const app = express();
@@ -8,5 +9,5 @@ app.use(express.json());
 
 app.use("/wavewise-backend", adminRoutes);
 app.use("/wavewise-backend", oceanDataRoutes);
-
+app.use("/", test);
 export const handler = serverless(app);
--- routes/routers.ts
+++ routes/routers.ts
@@ -2,13 +2,18 @@
 
 import express from "express";
 import { authenticateAdmin } from "../controller/authController";
-import {addOceanData, getOceanData} from "../controller/oceanDataController";
+import { addOceanData, getOceanData } from "../controller/oceanDataController";
 
 const router = express.Router();
 
 router.post("/admin/authenticate", authenticateAdmin);
 
-router.post('/ocean-data', addOceanData);
+router.post("/ocean-data", addOceanData);
 
-router.get('/ocean-data', getOceanData);
+router.get("/ocean-data", getOceanData);
+
+router.get("/wavewise-backend", (req, res) => {
+  res.json({ message: "Welcome to WaveWise Backend" });
+});
+
 export default router;
