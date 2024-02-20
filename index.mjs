cbf6c8224829b18be4edad512caf21f5a71ca4bb
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-20 17:32:25 -0500

    Commented out unused imports and routes

--- index.mjs
+++ index.mjs
@@ -1,14 +1,14 @@
 import express from "express";
-import adminRoutes from "./routes/routers.mjs";
-import oceanDataRoutes from "./routes/routers.mjs";
-import test from "./routes/routers.mjs";
+// import adminRoutes from "./routes/routers.mjs";
+// import oceanDataRoutes from "./routes/routers.mjs";
+// import test from "./routes/routers.mjs";
 import serverless from "serverless-http";
 
 const app = express();
 app.use(express.json());
 
-app.use("/wavewise-backend", adminRoutes);
-app.use("/wavewise-backend", oceanDataRoutes);
+// app.use("/wavewise-backend", adminRoutes);
+// app.use("/wavewise-backend", oceanDataRoutes);
 
 // app.use("/", test);
 
