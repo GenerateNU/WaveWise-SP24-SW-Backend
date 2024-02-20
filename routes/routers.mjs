7340b1bb1166efc29ebf25ebffce8dc2fb1f3b35
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-20 17:41:02 -0500

    Refactor routes and import statements

--- routes/routers.mjs
+++ routes/routers.mjs
@@ -1,19 +1,22 @@
 // routes/adminRoutes.ts
 
-import express from "express";
-import { authenticateAdmin } from "../controller/authController.mjs";
-import {
-  addOceanData,
-  getOceanData,
-} from "../controller/oceanDataController.mjs";
+// import express from "express";
+// import { authenticateAdmin } from "../controller/authController.mjs";
+// import {
+//   addOceanData,
+//   getOceanData,
+// } from "../controller/oceanDataController.mjs";
 
+// const router = express.Router();
+
+// router.post("/admin/authenticate", authenticateAdmin);
+
+// router.post("/ocean-data", addOceanData);
+
+// router.get("/ocean-data", getOceanData);
+
+const express = require("express");
 const router = express.Router();
-
-router.post("/admin/authenticate", authenticateAdmin);
-
-router.post("/ocean-data", addOceanData);
-
-router.get("/ocean-data", getOceanData);
 
 router.get("/wavewise-backend", (req, res) => {
   res.json({ message: "Welcome to WaveWise Backend" });
