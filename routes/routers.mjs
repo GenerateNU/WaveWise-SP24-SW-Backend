e56fdccd70a69acfe44f012bd2fa9594e3465889
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-20 23:54:13 -0500

    Commented out oceanDataController imports and routes

--- routes/routers.mjs
+++ routes/routers.mjs
@@ -1,16 +1,16 @@
 // routers.mjs
 import express from "express";
 // import { authenticateAdmin } from "../controller/authController.mjs";
-import {
-  addOceanData,
-  getOceanData,
-} from "../controller/oceanDataController.mjs";
+// import {
+//   addOceanData,
+//   getOceanData,
+// } from "../controller/oceanDataController.mjs";
 
 const router = express.Router();
 
 // router.post("/admin/authenticate", authenticateAdmin);
-router.post("/ocean-data", addOceanData);
-router.get("/ocean-data", getOceanData);
+// router.post("/ocean-data", addOceanData);
+// router.get("/ocean-data", getOceanData);
 
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to WaveWise Backend" });
