9831f9b1e0ca05287ae68b10335fe3e98f12e706
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-02-20 23:46:51 -0500

    Update route path in index.mjs

--- index.mjs
+++ index.mjs
@@ -1,15 +1,15 @@
 // index.mjs
 import express from "express";
-// import router from "./routes/routers.mjs";
+import router from "./routes/routers.mjs";
 import serverless from "serverless-http";
 
 const app = express();
 app.use(express.json());
 
-// app.use("/", router);
+app.use("/wavewise-backend", router);
 
-app.get("/wavewise-backend", (req, res) => {
-  res.json({ message: "Welcome to WaveWise Backend" });
-});
+// app.get("/wavewise-backend", (req, res) => {
+//   res.json({ message: "Welcome to WaveWise Backend" });
+// });
 
 export const handler = serverless(app);
