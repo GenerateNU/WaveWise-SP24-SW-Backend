a4883efd4f579db1ddc1b7497ea01373331896aa
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-04-03 18:41:09 -0400

    Refactor routes and import statements

--- index.mjs
+++ index.mjs
@@ -1,43 +1,16 @@
+// index.mjs
 import express from "express";
-import serverless from "serverless-http"; // Import serverless-http
-import {
-  addOceanData,
-  getOceanData,
-} from "./controller/oceanDataController.mjs";
-import {
-  signup,
-  confirmSignup,
-  login,
-  changePassword,
-  updateEmail,
-  logout,
-} from "./controller/authController.mjs";
+import router from "./routes/routers.mjs";
+import serverless from "serverless-http";
 
 const app = express();
 app.use(express.json());
 
-// Ocean Data routes
-app.post("/ocean-data", addOceanData);
+app.use("/", router);
 
-app.get("/ocean/", (req, res) => {
-  res.send("Hello from ocean!");
-});
-
-// Authentication routes
-app.post("/wavewise-backend/auth/signup", signup);
-app.post("/wavewise-backend/auth/confirm-signup", confirmSignup);
-app.post("/wavewise-backend/auth/login", login);
-app.post("/wavewise-backend/auth/change-password", changePassword);
-app.post("/wavewise-backend/auth/update-email", updateEmail);
-app.post("/wavewise-backend/auth/logout", logout);
-
-app.get("/wavewise-backend/", (req, res) => {
-  res.send("Hello from Express!");
-});
+export const handler = serverless(app);
 
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //   console.log(`Server listening on port ${PORT}`);
 // });
-
-export const handler = serverless(app);
