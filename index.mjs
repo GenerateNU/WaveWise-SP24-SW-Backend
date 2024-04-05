5a929fc28eef9a885f618a3babec58fdf2893f62
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2024-04-04 21:33:37 -0400

    test

--- index.mjs
+++ index.mjs
@@ -2,10 +2,11 @@
 import express from "express";
 import router from "./routes/routers.mjs";
 import serverless from "serverless-http";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/", router);
 
 export const handler = serverless(app);
